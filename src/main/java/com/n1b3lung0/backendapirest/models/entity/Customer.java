985b75d61e710a0527b60895212e691a9cadf51e
n1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-03-23 11:11:14 +0100

    initial commit

---
+++ src/main/java/com/n1b3lung0/backendapirest/models/entity/Customer.java
@@ -0,0 +1,29 @@
+package com.n1b3lung0.backendapirest.models.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String email;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/test/java/com/n1b3lung0/backendapirest/BackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n1b3lung0.backendapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
