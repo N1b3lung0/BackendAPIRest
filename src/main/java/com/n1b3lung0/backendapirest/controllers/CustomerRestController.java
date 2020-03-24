6a3f70f4e218015011905df699790c832fca7277
n1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-03-24 22:18:40 +0100

    crud with spring api rest

--- src/main/java/com/n1b3lung0/backendapirest/controllers/CustomerRestController.java
+++ src/main/java/com/n1b3lung0/backendapirest/controllers/CustomerRestController.java
@@ -3,10 +3,8 @@ package com.n1b3lung0.backendapirest.controllers;
 import com.n1b3lung0.backendapirest.models.entity.Customer;
 import com.n1b3lung0.backendapirest.models.services.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +20,25 @@ public class CustomerRestController {
     public List<Customer> index() {
         return customerService.findAll();
     }
+
+    @GetMapping("/customers/{id}")
+    public Customer show(@PathVariable Long id) { return customerService.findById(id); }
+
+    @PostMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer create(@RequestBody Customer customer) { return customerService.save(customer); }
+
+    @PutMapping("/customers/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer update(@RequestBody Customer customer, @PathVariable Long id) {
+        Customer currentCustomer = customerService.findById(id);
+        currentCustomer.setName(customer.getName());
+        currentCustomer.setSurname(customer.getSurname());
+        currentCustomer.setEmail(customer.getEmail());
+        return customerService.save(currentCustomer);
+    }
+
+    @DeleteMapping("customers/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) { customerService.delete(id); }
 }
--- src/main/java/com/n1b3lung0/backendapirest/models/entity/Customer.java
+++ src/main/java/com/n1b3lung0/backendapirest/models/entity/Customer.java
@@ -7,9 +7,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
-@Getter
-@Setter
-@Entity
+@Getter @Setter @Entity
 @Table(name = "customers")
 public class Customer implements Serializable {
 
@@ -24,6 +22,11 @@ public class Customer implements Serializable {
     @Column(name = "create_at")
     @Temporal(TemporalType.DATE)
     private Date createAt;
+
+    @PrePersist
+    public void prePersist() {
+        createAt = new Date();
+    }
 
     private static final long serialVersionUID = 1L;
 }
